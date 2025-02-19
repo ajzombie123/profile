7fa9f77204418371e396a2612f3b5a27cfbd497e
Auston <auston.yang@student.unsw.edu.au>
2025-02-20 00:28:46 +1100

    Removed large MOV file from Git tracking

---
+++ myprofile/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Header } from './Header';
+import { About } from './pages/About';
+import { Hobbies } from './pages/Hobbies';
+import { ChinaTrip } from './pages/ChinaTrip';
+import { Projects } from './pages/Projects';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<About />} />
+        <Route path='/Hobbies' element={<Hobbies />} />
+        <Route path='/Projects' element={<Projects />} />
+        <Route path='/ChinaTrip' element={<ChinaTrip />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
