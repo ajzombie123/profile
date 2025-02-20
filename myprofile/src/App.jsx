40bbd9ef64f24fc70859f37a042c66b9d352b8dd
Auston <auston.yang@student.unsw.edu.au>
2025-02-20 11:49:58 +1100

    argh

--- myprofile/src/App.jsx
+++ myprofile/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './Header';
@@ -9,8 +8,10 @@ import { Projects } from './pages/Projects';
 
 function App() {
 
+
   return (
     <BrowserRouter>
+      <div>hello</div>
       <Header></Header>
       <Routes>
         <Route path='/' element={<About />} />
@@ -19,7 +20,7 @@ function App() {
         <Route path='/ChinaTrip' element={<ChinaTrip />} />
       </Routes>
 
-    </BrowserRouter>
+  </BrowserRouter>
   )
 }
 
